66c0980891b1dc9f6ea571bdc7d1f0ef52efd0cd
Korpi <da.dnvl@live.com>
2022-05-09 18:52:05 -0500

    Solves excercises 5.4

---
+++ 5_4_Excercises.py
@@ -0,0 +1,9 @@
+#Create some Python code that will prompt you to enter your name. Print out Hello and then the name.
+
+name = input("What's your name\n")
+print(f"Hello, {name}.")
+
+#Create a program that will ask a user how old they are. Print out some text telling them how old they will be next year.
+age = int(input("How old are you?\n"))
+print(f"You'll be {age + 1} next year!")
+print(f"That's it for now. Bye")
